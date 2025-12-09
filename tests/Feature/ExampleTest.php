f58812176d224ce075990decc8152a18d444b5dd
Haevol <info@haevol.org>
2025-12-09 15:07:08 +0100

    Add PHPUnit configuration and basic test structure

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Haevol\OpenProjectFeedback\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
